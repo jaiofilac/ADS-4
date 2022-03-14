0e7f78c505ea313bf013e9497219dfbf4584a396
Андрей Жаворонков <96244220+jaiofilac@users.noreply.github.com>
2022-03-14 21:38:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,71 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+
+int cbinsearch(int* arr, int size, int value) {
+  int current = size - 1;
+  int step = size;
+  int result = 0;
+  while (true) {
+    step = step >> 1;
+    if (value < arr[current]) {
+      current -= step;
+    } else if (value > arr[current]) {
+      current += step;
+    } else if (value == arr[current]) {
+      for (; value == arr[current]; current++) {}
+      current--;
+      for (; value == arr[current] && current >= 0; current--) {
+        result++;
+      }
+      break;
+    }
+    if (step == 0) {
+      break;
+    }
+  }
+  return result;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+
+int countPairs1(int* arr, int len, int value) {
+  int result = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        result++;
+      }
+    }
+  }
+  return result;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+
+int countPairs2(int* arr, int len, int value) {
+  int lim = value / 2;
+  int result = 0;
+  for (int i = 0; arr[i] <= lim && i < len; i++) {
+    bool found = false;
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        found = true;
+        result++;
+        continue;
+      }
+      if (found) {
+        break;
+      }
+    }
+  }
+  return result;
 }
+
+int countPairs3(int* arr, int len, int value) {
+  int lim = value / 2;
+  int result = 0;
+  int halfAmount = cbinsearch(arr, len, lim);
+  for (int i = 0; arr[i] <= lim && i < len; i++) {
+    result += cbinsearch(arr, len, value - arr[i]);
+  }
+  for (int i = 1; i <= halfAmount; i++) {
+    result -= i;
+  }
+  return result;
+}
+
